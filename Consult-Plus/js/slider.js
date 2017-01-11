5424cbd613a7928fce0a8b0b898c18ab0806392c
holmatro90 <holmatro686@gmail.com>
2017-01-11 20:03:14 +0200

    Add some prew work

---
+++ Consult-Plus/js/slider.js
@@ -0,0 +1,48 @@
+
+$(document).ready( function() {
+    $('#carousel-example-generic').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#carousel-example-generic').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
+
+$(document).ready( function() {
+    $('#carousel-generic').carousel({
+		interval:   4000
+	});
+	
+	var clickEvent = false;
+	$('#carousel-generic').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
